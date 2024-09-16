7b3b9aba5e87c5e84e37083359a0861c9a6a94df
Abby Waddell <wabby.404@gmail.com>
2024-09-16 14:02:24 -0400

    V model and custom component events practice

--- src/components/EmittingPractice.vue
+++ src/components/EmittingPractice.vue
@@ -3,8 +3,6 @@
 </template>
 
 <script setup>
-import { defineEmits } from "vue";
-
 const emit = defineEmits(["customEvent"]);
 function notifyParent() {
   emit("customEvent", { message: "Hello from child." });
