b8d0d5dc9e56ba948fb4adc37ae4b7db3fa80b2b
Abby Waddell <wabby.404@gmail.com>
2024-09-15 21:17:07 -0400

    Emit validation & emit changing a ref

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <h2>This is a popup</h2>
-    <button @click="$emit('close', 1)">Close Popup</button>
+    <input type="text" v-model="name" />
+    <button @click="$emit('close', name)">Close Popup</button>
     <!-- this second value will be the value passed up to the parent, if needed. can be omitted -->
   </div>
 </template>
 
-<script></script>
+<script>
+import { ref } from "vue";
+
+const name = ref("");
+</script>
 
 <style scoped></style>
